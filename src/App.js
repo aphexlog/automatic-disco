b9fad2abecdf2f9a57d2ec6c7491ce97ebab8fac
billy <aphexlog@gmail.com>
2022-10-22 19:15:11 -0500

    adding login

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
-// import logo from './logo.svg';
-import * as mui from '@mui/material';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <mui.Box style={
-      {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        backgroundColor: '#282c34',
-        color: 'white',
-      }
-    }>
-      <div
-      // center the content
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      }}
-      >
-        <mui.TextField
-          id="NameField"
-          label="What's your name?"
-          variant="outlined"
-          sx={{ m: 1, width: '30ch' }}
-          onClick={() => console.log('clicked')}
-          color="secondary"
-          />
-      </div>
-    </mui.Box>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
